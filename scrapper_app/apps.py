a687d103486e012793b7b0a53c0c53cbddadfe47
kopilaaa <kopilatharu25@gmail.com>
2019-12-18 17:29:41 +0545

    at the dnd of day 3

---
+++ scrapper_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ScrapperAppConfig(AppConfig):
+    name = 'scrapper_app'
---
+++ scrapper_app/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def index(request):
+     return render(request,'scrapper_app/index.html')
+
+
+
+
+
+
+
+
+
+
+    
+# def kopil(requests):
+
+#     return HttpResponse('i am kopila')     
+
